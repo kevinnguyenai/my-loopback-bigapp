285ea81182964ca9704bbe1078fbd06a5f2cb563
kevinnguyenai <kevin.nguyen.ai@gmail.com>
2021-11-22 18:08:14 +0900

    refactor: fix eslint and prettier
    Signed-off-by: kevinnguyenai <kevin.nguyen.ai@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['./packages/eslint-config/eslintrc.js', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      rules: {
+        /*
+         * The mocha plugin reports the following signature as a violation of
+         * `mocha/handle-done-callback` (misinterpreting `this` as `done`).
+         *
+         * See https://github.com/lo1tuma/eslint-plugin-mocha/issues/270
+         *
+         * ```ts
+         * before(async function setupApplication(this: Mocha.Context) {
+         *   this.timeout(6000);
+         *   // ...
+         * }
+         * ```
+         */
+        'mocha/handle-done-callback': 'off',
+      },
+    },
+  ],
+};
